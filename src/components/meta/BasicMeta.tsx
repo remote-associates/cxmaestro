888f03717983bba1b1f55609c997c5a955c44b14
remote-associates <68965239+remote-associates@users.noreply.github.com>
2021-08-24 12:07:41 -0500

    add site identity to head of main index

--- src/components/meta/BasicMeta.tsx
+++ src/components/meta/BasicMeta.tsx
@@ -34,6 +34,7 @@ export default function BasicMeta({
       />
       {author ? <meta name="author" content={author} /> : null}
       <link rel="canonical" href={config.base_url + url} />
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     </Head>
   );
 }
